dd0f269a0100cb14128a920a175e64495db5e3f7
Camilo Santander <camilo.santander1991@gmail.com>
2022-08-09 20:27:10 -0400

    Curso Flutter 80

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -16,7 +16,18 @@ class CardScreen extends StatelessWidget {
         children: const [
           CustomCardType1(),
           SizedBox(height: 10),
-          CustomCardType2(),
+          CustomCardType2(
+              imageUrl:
+                  'https://mymodernmet.com/wp/wp-content/uploads/2022/02/international-landscape-photographer-awards-20.jpeg'),
+          SizedBox(height: 10),
+          CustomCardType2(
+              imageUrl:
+                  'https://media-exp2.licdn.com/dms/image/C561BAQGEbvT3SFyR9Q/company-background_10000/0/1582050035728?e=2147483647&v=beta&t=lxt09r99nB70MdZRGsnwgcBbBYkPuiIEIRf2Vk9lSb8'),
+          SizedBox(height: 10),
+          CustomCardType2(
+              imageUrl:
+                  'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg'),
+          SizedBox(height: 100),
         ],
       ),
     );
--- lib/widgets/custom_card_type_2.dart
+++ lib/widgets/custom_card_type_2.dart
@@ -1,18 +1,33 @@
+import 'package:fl_components/themes/app_theme.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class CustomCardType2 extends StatelessWidget {
-  const CustomCardType2({super.key});
+  final String imageUrl;
+
+  const CustomCardType2({super.key, required this.imageUrl});
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      elevation: 10,
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
+      elevation: 30,
+      shadowColor: AppTheme.primary.withOpacity(.5),
       child: Column(
-        children: const [
+        children: [
           FadeInImage(
-            image: NetworkImage(
-                'https://mymodernmet.com/wp/wp-content/uploads/2022/02/international-landscape-photographer-awards-20.jpeg'),
-            placeholder: AssetImage('lib/assets/jar-loading.gif'),
+            image: NetworkImage(imageUrl),
+            placeholder: const AssetImage('lib/assets/jar-loading.gif'),
+            width: double.infinity,
+            height: 230,
+            fit: BoxFit.cover,
+            fadeInDuration: const Duration(milliseconds: 200),
+          ),
+          Container(
+            alignment: AlignmentDirectional.centerEnd,
+            padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
+            child: const Text('Un hermoso paisaje'),
           )
         ],
       ),
